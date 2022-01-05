d3cec652425a6aaf5b776344640d2ed8e7447b72
Chirag-Khandelwal <github@mail.chiragkh.me>
2022-01-05 15:03:12 +0530

    added code coverage stuff - use 'make scribe_cov'

---
+++ codecov.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+CWD=$(pwd)
+SCRIBE=build/bin/scribe
+
+cd $1
+
+# errors
+## no file provided
+echo "$SCRIBE"
+$SCRIBE >/dev/null 2>&1
+## file not found
+echo "$SCRIBE no_file.sc"
+$SCRIBE no_file.sc >/dev/null 2>&1
+## compilation
+echo "$SCRIBE error_examples/compile_error.sc"
+$SCRIBE error_examples/compile_error.sc >/dev/null 2>&1
+
+# all examples
+for x in examples/*; do echo "$SCRIBE $x"; $SCRIBE $x -s >/dev/null; rm $(basename ${x%.*}); done
+
+# version info
+echo "$SCRIBE -v"
+$SCRIBE -v >/dev/null 2>&1
+
+# help
+echo "$SCRIBE --help"
+$SCRIBE --help >/dev/null 2>&1
+
+cd $CWD
