30173f0b1bd1aea5f07b7bdd7c4659032e89bc06
Chirag-Khandelwal <github@mail.chiragkh.me>
2022-01-06 20:34:12 +0530

    minor change in codecov.sh

--- codecov.sh
+++ codecov.sh
@@ -16,9 +16,6 @@ $SCRIBE no_file.sc >/dev/null 2>&1
 echo "$SCRIBE error_examples/compile_error.sc"
 $SCRIBE error_examples/compile_error.sc >/dev/null 2>&1
 
-# all examples
-for x in examples/*; do echo "$SCRIBE $x"; $SCRIBE $x -s >/dev/null; rm $(basename ${x%.*}); done
-
 # version info
 echo "$SCRIBE -v"
 $SCRIBE -v >/dev/null 2>&1
@@ -26,5 +23,8 @@ $SCRIBE -v >/dev/null 2>&1
 # help
 echo "$SCRIBE --help"
 $SCRIBE --help >/dev/null 2>&1
+
+# all examples
+for x in examples/*; do echo "$SCRIBE $x"; $SCRIBE $x -s >/dev/null; rm $(basename ${x%.*}); done
 
 cd $CWD
